5a9cf614bb205ea65fddafbb1e2a7e353876e7ca
William <williameliason8@gmail.com>
2021-02-05 13:42:13 -0600

    users and rentals seeding function complete

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -2,7 +2,9 @@ const faker = require('faker');
 const fs = require('fs');
 
 const writeUserCols = fs.createWriteStream('server/db/csvData/users.csv');
-writeUserCols.write('name,avatar\n', 'utf8');
+//writeUserCols.write('name,avatar\n', 'utf8');
+const writeRentalCols = fs.createWriteStream('server/db/csvData/rentals.csv');
+
 
 const writeUserRows = (writer, encoding, callback) => {
   let i = 1000
@@ -26,4 +28,36 @@ const writeUserRows = (writer, encoding, callback) => {
   write();
 }
 
+let continents = ['noam','noam','noam','eu','eu','soam','asia','asia','asia','aus','aus']
+const writeRentalRows = (writer, encoding, callback) => {
+  let i = 1000;
+  let ok = true;
+  const write = () => {
+    do {
+        i-=1;
+        const name = faker.fake("{{vehicle.color}} {{commerce.productName}} {{address.city}}")
+        const continent = continents[Math.floor(Math.random()*11)];
+        const data = `${name},${continent}\n`;
+        if (i===0) {
+          writer.write(data, encoding, callback);
+        } else {
+          ok = writer.write(data, encoding);
+        }
+    } while (i>0 && ok);
+    if (i>0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
 writeUserRows(writeUserCols, 'utf-8', ()=> {writeUserCols.end();});
+writeRentalRows(writeRentalCols, 'utf-8', ()=> {writeRentalCols.end();});
+
+/*                          SEED USERS COMMAND */
+///Users/williameliason/sdc/sdc-review-component/server/db/csvData/users.csv
+// COPY users (name, avatar) FROM '/Users/williameliason/sdc/sdc-review-component/server/db/csvData/users.csv' WITH delimiter ',';
+
+/*                          SEED RENTALS COMMAND */
+///Users/williameliason/sdc/sdc-review-component/server/db/csvData/rentals.csv
+// COPY rentals (name, continent) from '/Users/williameliason/sdc/sdc-review-component/server/db/csvData/rentals.csv' WITH delimiter ',';
